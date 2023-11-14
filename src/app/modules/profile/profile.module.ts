f0e6b557b3e60af1ac37a8a9e9f5648733aff7a2
Nicolás  Vercellone <85401681+biltox@users.noreply.github.com>
2023-11-14 08:43:59 -0300

    Fix: problemas generales

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UserEditComponent } from './user-edit/user-edit.component';
+
 
 
 @NgModule({
   declarations: [
     UserProfileComponent,
-    UserEditComponent,
   ],
   imports: [
     CommonModule,
